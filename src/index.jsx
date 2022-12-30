05f679258fed817836e6a60fd7399e16c6ce5c52
Arseniy Oguzov <oguzov.a@n-t.io>
2022-12-30 04:41:32 +0300

    fix service worker

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,11 @@ import { render } from 'solid-js/web';
 import './index.css';
 import App from './components/App';
 
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/serviceworker.js')
+        .then(() => { console.log('Service Worker Registered'); });
+}
+
 render(() => <App />, document.getElementById('root'));
