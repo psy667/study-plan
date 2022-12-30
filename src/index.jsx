7362df3e01f41d5d0dcb188445c7603975062279
Arseniy Oguzov <oguzov.a@n-t.io>
2022-12-30 04:02:32 +0300

    add serviceworker

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,8 @@ import { render } from 'solid-js/web';
 import './index.css';
 import App from './components/App';
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("/assets/serviceworker.js");
+}
+
 render(() => <App />, document.getElementById('root'));
